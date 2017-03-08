1dff22f8f35097fb70106e4d73cd0b14578739ec
Martin Michalko <matemichal@gmail.com>
2017-03-08 19:32:11 +0100

    k8 installed and activated for all nodes

--- startpbs.sh
+++ startpbs.sh
@@ -9,5 +9,5 @@ ansible-playbook -i inventory packages-req.yml
 ansible-playbook -i inventory all.yml
 #create custom snapshot - fist should be all scripts finished and already
 # after then the snapshot can be created
-ansible-playbook -i inventory /data/projects/ansible2-2/pb/virtuale/snapshot-create-custom.yml --extra-vars "@group_vars/all.yml" -e 'ansible_python_interpreter=/usr/bin/python'
+ansible-playbook -i inventory /data/projects/ansible2-2/pb/virtuale/snapshot-create-custom.yml --extra-vars "@group_vars/all.yml"
 #we can continue
